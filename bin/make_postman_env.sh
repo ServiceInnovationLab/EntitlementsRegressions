200d6701c08911ebd8d277e1b8784c5ac64cae97
Brenda Wallace <brenda@wallace.net.nz>
2018-01-19 12:20:16 +1300

    Adding Postman env

---
+++ bin/make_postman_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILENAME=./postman_env.json
+
+cat > $FILENAME <<- EndOfConfig
+{
+  "id": "0d418806-6615-a7f1-a100-d6e008753d7f",
+  "name": "RaaP API Env",
+  "values": [
+    {
+      "enabled": true,
+      "key": "token\n",
+      "value": "$TOKEN",
+      "type": "text"
+    }
+  ],
+  "timestamp": 1516309420735,
+  "_postman_variable_scope": "environment",
+  "_postman_exported_at": "2018-01-18T22:43:03.675Z",
+  "_postman_exported_using": "Postman/5.5.0"
+}
+EndOfConfig
