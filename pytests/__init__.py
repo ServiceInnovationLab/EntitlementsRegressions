4af7c335be7b8aa7d54b2ba1fdbe166b8d7cc5cf
Brenda Wallace <brenda@wallace.net.nz>
2018-01-23 13:08:04 +1300

    Moved base url to variable

--- pytests/__init__.py
+++ pytests/__init__.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv, find_dotenv
 # Load config from .env
 load_dotenv(find_dotenv())
 TOKEN = os.environ.get('RAAP_TOKEN')
+BASE_URL = 'https://nz.raap.d61.io/api/v0/domain/nz-entitlements-eligibility'
 
 class Reasoner(unittest.TestCase):
     def runReason(self, body):
-        url = 'https://nz.raap.d61.io/api/v0/domain/nz-entitlements-eligibility/reasoning/reason?criteria=draft'
+        url = '{base_url}/reasoning/reason?criteria=draft'.format(base_url=BASE_URL)
         headers = {'Content-Type': 'application/json',
                    'Authorization': 'Bearer {token}'.format(token=TOKEN)}
         return requests.post(url, headers=headers, data=json.dumps(body))
