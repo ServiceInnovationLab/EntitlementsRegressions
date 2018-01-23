077badfc48d9f4c24137a9530d7d215edaa7bd33
Brenda Wallace <brenda@wallace.net.nz>
2018-01-23 13:08:13 +1300

    code style fix up

--- pytests/__init__.py
+++ pytests/__init__.py
@@ -9,9 +9,12 @@ load_dotenv(find_dotenv())
 TOKEN = os.environ.get('RAAP_TOKEN')
 BASE_URL = 'https://nz.raap.d61.io/api/v0/domain/nz-entitlements-eligibility'
 
+
 class Reasoner(unittest.TestCase):
+
     def runReason(self, body):
-        url = '{base_url}/reasoning/reason?criteria=draft'.format(base_url=BASE_URL)
+        url = '{base_url}/reasoning/reason?criteria=draft'.format(
+            base_url=BASE_URL)
         headers = {'Content-Type': 'application/json',
                    'Authorization': 'Bearer {token}'.format(token=TOKEN)}
         return requests.post(url, headers=headers, data=json.dumps(body))
