3265b449d6c28156b25570961eaa3b22ef8b6b64
Dana Iti <dana.iti.work@gmail.com>
2018-01-26 10:50:42 +1300

    Added unsupported childs benefit

---
+++ pytests/test_unsupported_childs_benefit.py
@@ -0,0 +1,35 @@
+from . import Reasoner
+
+"""
+Benefit: Part 1C Unsupported Child's Benefit (eligible):
+If applicant.isPrincipalCarerForOneYearFromApplicationDate
+    and parents.areUnableToProvideSufficientCare
+    and 18 ≤ applicant.Age
+    and not applicant.isParent
+    and applicant.isNZResident
+    and child.isDependent
+        then benefit.isUnsupportedChildsBenefit is PERMITTED
+"""
+
+
+class TestUnsupportedChildsBenefit(Reasoner):
+    key = 'isUnsupportedChildsBenefit'
+
+    body = {
+        "applicant": {
+            "isPrincipalCarerForOneYearFromApplicationDate": True,
+            "Age": 19,
+            "isParent": False,
+            "isNZResident": True
+        },
+        "parents": {
+            "areUnableToProvideSufficientCare": True
+        },
+        "child": {
+            "isDependent": True
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_conclusive)
+        self.assertTrue(self.is_permitted)
