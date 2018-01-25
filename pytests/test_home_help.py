8f1cd329cfaf5028610b531acb75dfe1e1ca6c1f
Dana Iti <dana.iti.work@gmail.com>
2018-01-25 15:04:20 +1300

    Home help test added

--- pytests/test_home_help.py
+++ pytests/test_home_help.py
@@ -1,22 +1,28 @@
 from . import Reasoner
 
+"""
+Benefit: Home Help (eligibility in policy for applicant with disability):
+    If applicant.isNZResident
+        and applicant.normallyLivesInNZ
+        and applicant.needsDomesticSupport
+        and applicant.hasSeriousDisability
+        and threshold.cash.HomeHelp
+        and applicant.holdsCommunityServicesCard
+            then benefit.isHomeHelp is PERMITTED
+"""
 
 class TestHomeHelp(Reasoner):
     key = 'isHomeHelp'
 
     body = {
         "applicant": {
-            "Age": 30,
-            "normallyLivesInNZ": True,
             "isNZResident": True,
-            "hasAccommodationCosts": True,
-            "hasSocialHousing": False,
-            "receivesAccommodationSupport": False
+            "normallyLivesInNZ": True,
+            "needsDomesticSupport": True,
+            "hasSeriousDisability": True,
+            "holdsCommunityServicesCard": True
         },
         "threshold": {
-            "income": {
-                "HomeHelp": True
-            },
             "cash": {
                 "HomeHelp": True
             }
