fe1f51dc2d4156d3c1d35e63c9263234894e2201
Dana Iti <dana.iti.work@gmail.com>
2018-01-26 13:13:32 +1300

    Added Young Parent Support test

---
+++ pytests/test_young_parent_support.py
@@ -0,0 +1,40 @@
+from . import Reasoner
+
+"""
+Benefit: Young Parent Payment (eligibility):
+    If 16 ≤ applicant.Age
+        and applicant.Age ≤ 19
+        and child.isDependent
+        and applicant.isNZResident
+        and applicant.normallyLivesInNZ
+        and applicant.isParent
+        and not benefit.isOrphansBenefit
+        and not benefit.isUnsupportedChildsBenefit
+        and 1 ≤ applicant.numberOfChildren
+            then benefit.isYoungParentPayment is PERMITTED
+"""
+
+
+class TestYoungParentSupport(Reasoner):
+    key = 'isYoungParentPayment'
+
+    body = {
+        "applicant": {
+            "Age": 16,
+            "isNZResident": True,
+            "normallyLivesInNZ": True,
+            "isParent": True,
+            "numberOfChildren": 1
+        },
+        "child": {
+            "isDependent": True
+        },
+        "benefit": {
+            "isOrphansBenefit": False,
+            "isUnsupportedChildsBenefit": False
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_conclusive)
+        self.assertTrue(self.is_permitted)
