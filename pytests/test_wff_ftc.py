9852bec88737173d8b0d91330fa904af534c1897
Dana Iti <dana.iti.work@gmail.com>
2018-01-29 10:57:05 +1300

    tests if result is conclusively forbidden

--- pytests/test_wff_ftc.py
+++ pytests/test_wff_ftc.py
@@ -1,72 +1,110 @@
+# from . import Reasoner
+# from nose import SkipTest
+
+# """
+# Benefit: Working for Families - Family Tax Credit (eligibility):
+# If applicant.isParent
+#     and 20 ≤ applicant.worksWeeklyHours
+#     and not applicant.receivesIncomeTestedBenefit
+#     and not benefit.isStudentAllowance
+#     and applicant.isPrincipalCarer
+#     and not benefit.isOrphansBenefit is PERMITTED
+#     and not benefit.isUnsupportedChildsBenefit is PERMITTED
+#         then benefit.isWorkingForFamiliesFamilyTaxCredit is PERMITTED
+# """
+
+
+# class TestWorkingForFamiliesFamilyTaxCredit(Reasoner):
+
+#     # TODO: Add enough info to cause
+#     # isOrphansBenefit isUnsupportedChildsBenefit
+#     # to not be permitted
+
+#     key = 'isWorkingForFamiliesFamilyTaxCredit'
+#     body = {
+#         "applicant": {
+#             "isParent": True,
+#             "worksWeeklyHours": 19,
+#             "receivesIncomeTestedBenefit": False,
+#             "isPrincipalCarer": True
+#         },
+#         "benefit": {
+#             "isStudentAllowance": False,
+#         },
+#         "threshold": {
+#             "income": {
+#                 "AccommodationSupplement": True
+#             },
+#             "cash": {
+#                 "AccommodationSupplement": True
+#             }
+#         }
+#     }
+
+#     @SkipTest
+#     def test_reasoning(self):
+#         self.assertTrue(self.is_conclusive)
+#         self.assertTrue(self.is_permitted)
+
+
+# class TestWorkingForFamiliesFamilyTaxCreditNotEnoughInfo(Reasoner):
+
+#     key = 'isWorkingForFamiliesFamilyTaxCredit'
+#     body = {
+#         "applicant": {
+#             "Age": 30,
+#             "normallyLivesInNZ": True,
+#             "isNZResident": True,
+#             "hasAccommodationCosts": True,
+#             "hasSocialHousing": False,
+#             "receivesAccommodationSupport": False
+#         },
+#         "threshold": {
+#             "income": {
+#                 "AccommodationSupplement": True
+#             }
+#         }
+#     }
+
+#     @SkipTest
+#     def testNotEnoughInfo(self):
+#         print(self.findConclusiveReasoningResult())
+#         self.assertFalse(self.is_conclusive)
 from . import Reasoner
-from nose import SkipTest
 
 """
 Benefit: Working for Families - Family Tax Credit (eligibility):
-If applicant.isParent
+    If applicant.isParent
     and 20 ≤ applicant.worksWeeklyHours
     and not applicant.receivesIncomeTestedBenefit
     and not benefit.isStudentAllowance
     and applicant.isPrincipalCarer
     and not benefit.isOrphansBenefit is PERMITTED
-    and not benefit.isUnsupportedChildsBenefit is PERMITTED
         then benefit.isWorkingForFamiliesFamilyTaxCredit is PERMITTED
 """
 
 
 class TestWorkingForFamiliesFamilyTaxCredit(Reasoner):
-
-    # TODO: Add enough info to cause
-    # isOrphansBenefit isUnsupportedChildsBenefit
-    # to not be permitted
-
     key = 'isWorkingForFamiliesFamilyTaxCredit'
+
     body = {
         "applicant": {
             "isParent": True,
-            "worksWeeklyHours": 19,
+            "worksWeeklyHours": 20,
             "receivesIncomeTestedBenefit": False,
-            "isPrincipalCarer": True
+            "isPrincipalCarer": True,
+            "hasSocialHousing": False,
+            "normallyLivesInNZ": True,
+            "isNZResident": True
+        },
+        "child": {
+            "isDependent": True
         },
         "benefit": {
             "isStudentAllowance": False,
-        },
-        "threshold": {
-            "income": {
-                "AccommodationSupplement": True
-            },
-            "cash": {
-                "AccommodationSupplement": True
-            }
+            "isOrphansBenefit": False
         }
     }
 
-    @SkipTest
     def test_reasoning(self):
-        self.assertTrue(self.is_conclusive)
-        self.assertTrue(self.is_permitted)
-
-
-class TestWorkingForFamiliesFamilyTaxCreditNotEnoughInfo(Reasoner):
-
-    key = 'isWorkingForFamiliesFamilyTaxCredit'
-    body = {
-        "applicant": {
-            "Age": 30,
-            "normallyLivesInNZ": True,
-            "isNZResident": True,
-            "hasAccommodationCosts": True,
-            "hasSocialHousing": False,
-            "receivesAccommodationSupport": False
-        },
-        "threshold": {
-            "income": {
-                "AccommodationSupplement": True
-            }
-        }
-    }
-
-    @SkipTest
-    def testNotEnoughInfo(self):
-        print(self.findConclusiveReasoningResult())
-        self.assertFalse(self.is_conclusive)
+        self.assertTrue(self.is_conclusively_forbidden)
