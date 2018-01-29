227e9db4d24ae4f7dedd8302fec4c71cd5505370
Dana Iti <dana.iti.work@gmail.com>
2018-01-29 14:14:42 +1300

    Pep8 cleanup

--- pytests/test_wff_ftc.py
+++ pytests/test_wff_ftc.py
@@ -1,75 +1,61 @@
 from . import Reasoner
-from nose import SkipTest
 
 """
 Benefit: Working for Families - Family Tax Credit (eligibility):
-If applicant.isParent
-    and 20 ≤ applicant.worksWeeklyHours
-    and not applicant.receivesIncomeTestedBenefit
-    and not benefit.isStudentAllowance
-    and applicant.isPrincipalCarer
-    and not benefit.isOrphansBenefit is PERMITTED
-    and not benefit.isUnsupportedChildsBenefit is PERMITTED
-        then benefit.isWorkingForFamiliesFamilyTaxCredit is PERMITTED
+    If applicant.isParent
+        and 20 ≤ applicant.worksWeeklyHours
+        and not applicant.receivesIncomeTestedBenefit
+        and not benefit.isStudentAllowance
+        and applicant.isPrincipalCarer
+        and not benefit.isOrphansBenefit
+            then benefit.isWorkingForFamiliesFamilyTaxCredit is PERMITTED
+
+Orphans Benefit should return FORBIDDEN
+    If child.isDependent
+        and parents.areDeceasedMissingOrIncapableThroughDisability
+        and applicant.isPrincipalCarerForOneYearFromApplicationDate
+        and 18 ≤ applicant.age
+        and not applicant.isParent
+        and applicant.isNZResident
+        and applicant.normallyLivesInNZ
+            then benefit.isOrphansBenefit is PERMITTED
+
+isStudentAllowance should return FORBIDDEN
+    If applicant.isNZResident
+        and applicant.normallyLivesInNZ
+        and applicant.age ≤ 65
+        and 18 ≤ applicant.age
+        and applicant.isStudyingFullTime
+        and not applicant.receivesIncomeTestedBenefit
+            then benefit.isStudentAllowance is PERMITTED
 """
 
 
 class TestWorkingForFamiliesFamilyTaxCredit(Reasoner):
 
-    # TODO: Add enough info to cause
-    # isOrphansBenefit isUnsupportedChildsBenefit
-    # to not be permitted
-
     key = 'isWorkingForFamiliesFamilyTaxCredit'
+
     body = {
         "applicant": {
             "isParent": True,
-            "worksWeeklyHours": 19,
+            "worksWeeklyHours": 20,
             "receivesIncomeTestedBenefit": False,
-            "isPrincipalCarer": True
+            "isPrincipalCarer": True,
+            "isPrincipalCarerForOneYearFromApplicationDate": False,
+            "isStudyingFullTime": False
         },
         "benefit": {
             "isStudentAllowance": False,
+            "isOrphansBenefit": False
         },
-        "threshold": {
-            "income": {
-                "AccommodationSupplement": True
-            },
-            "cash": {
-                "AccommodationSupplement": True
-            }
+        "child": {
+            "isDependent": False
+        },
+        "parents": {
+            "areDeceasedMissingOrIncapableThroughDisability": False
         }
     }
 
-    def test_reasoning(self):
-        self.assertTrue(self.is_conclusively_forbidden)
-
-    @SkipTest
     def test_reasoning(self):
         self.assertTrue(self.is_conclusive)
-        self.assertTrue(self.is_permitted)
-
-
-class TestWorkingForFamiliesFamilyTaxCreditNotEnoughInfo(Reasoner):
-
-    key = 'isWorkingForFamiliesFamilyTaxCredit'
-    body = {
-        "applicant": {
-            "Age": 30,
-            "normallyLivesInNZ": True,
-            "isNZResident": True,
-            "hasAccommodationCosts": True,
-            "hasSocialHousing": False,
-            "receivesAccommodationSupport": False
-        },
-        "threshold": {
-            "income": {
-                "AccommodationSupplement": True
-            }
-        }
-    }
-
-    @SkipTest
-    def testNotEnoughInfo(self):
-        print(self.findConclusiveReasoningResult())
-        self.assertFalse(self.is_conclusive)
+        self.assertTrue(self.is_conclusively_forbidden)
