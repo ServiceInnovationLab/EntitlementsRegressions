426dcad86ab84fc60c669f6854168dd82e8ba8cd
Brenda Wallace <brenda@wallace.net.nz>
2018-02-16 10:32:32 +1300

    Removed duplicate test

--- pytests/test_wff_family_tax_credit.py
+++ pytests/test_wff_family_tax_credit.py
@@ -46,32 +46,3 @@ class TestWFFTaxCredit(TestWFFTaxCreditKey):
     def test_reasoning(self):
         self.assertTrue(self.is_permitted)
         self.assertTrue(self.is_conclusive)
-
-
-class TestWFFTaxCredit(TestWFFTaxCreditKey):
-
-    """
-    Benefit: Working for Families - Family Tax Credit (eligibility):
-        If applicant.isParent
-        and applicant.isPrincipalCarer
-        and not benefit.isOrphansBenefit
-        and not benefit.isUnsupportedChildsBenefit
-        and not benefit.isFosterCareAllowance
-        then benefit.isWorkingForFamiliesFamilyTaxCredit is PERMITTED
-    """
-
-    body = {
-        "applicant": {
-            "isParent": True,
-            "isPrincipalCarer": True
-        },
-        "benefit": {
-            "isOrphansBenefit": False,
-            "isUnsupportedChildsBenefit": False,
-            "isFosterCareAllowance": False
-        }
-    }
-
-    def test_reasoning(self):
-        self.assertTrue(self.is_permitted)
-        self.assertTrue(self.is_conclusive)
