e8126d4ee7e75bb9582c9bf027348779b03001ba
Brenda Wallace <shiny@cpan.org>
2018-02-28 15:00:09 +1300

    More tests of WFF PTC when other entitlements or ACC

--- pytests/test_wff_parental_tax_credit.py
+++ pytests/test_wff_parental_tax_credit.py
@@ -22,6 +22,13 @@ Applicant is NOT eligible if they:
 """
 
 
+class TestWFF_ParentalTaxCreditDefault(TestKey):
+    body = {}
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_forbidden)
+
+
 class TestWFF_ParentalTaxCredit(TestKey):
     body = {
         "applicant": {
@@ -32,3 +39,85 @@ class TestWFF_ParentalTaxCredit(TestKey):
 
     def test_reasoning(self):
         self.assertTrue(self.is_permitted)
+
+
+class TestWFF_ParentalLessChildcare(TestKey):
+    body = {
+        "applicant": {
+            "isPrincipalCarerForProportion": 4,
+            "receivesIncomeTestedBenefit": False
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_forbidden)
+
+
+class TestWFF_ParentalTaxCreditOnMTBenefit(TestKey):
+    body = {
+        "applicant": {
+            "isPrincipalCarerForProportion": 100,
+            "receivesIncomeTestedBenefit": True
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_forbidden)
+
+
+class TestWFF_ParentalTaxCreditForbiddenOnAcc(TestKey):
+    body = {
+        "applicant": {
+            "isPrincipalCarerForProportion": 34,
+            "receivesIncomeTestedBenefit": False,
+            "isOnACCCompensation": True
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_forbidden)
+
+
+class TestWFF_ParentalTaxCreditForbiddenOrphans(TestKey):
+    body = {
+        "applicant": {
+            "isPrincipalCarerForProportion": 34,
+            "receivesIncomeTestedBenefit": False,
+            # add the orphans bene requirements
+            "Age": 87,
+            "isNZResident": True,
+            "isParent": False,
+            "isPrincipalCarerForOneYearFromApplicationDate": True,
+            "normallyLivesInNZ": True
+        },
+        "child": {
+            "isDependent": True
+        },
+        "parents": {
+            "areDeceasedMissingOrIncapable": True
+        }
+    }
+
+    def test_reasoning(self):
+        # Orphans benefit is permitted, so WFF PTC is not
+        self.assertTrue(self.isPermitted('isOrphansBenefit'))
+        self.assertTrue(self.is_forbidden)
+
+
+class TestWFF_ParentalTaxCreditForbiddenStudents(TestKey):
+    body = {
+        "applicant": {
+            "isPrincipalCarerForProportion": 34,
+            "receivesIncomeTestedBenefit": False,
+            # add the student allowance requirements
+            "isNZResident": True,
+            "normallyLivesInNZ": True,
+            "Age": 25,
+            "isStudyingFullTime": True,
+        }
+    }
+
+    def test_reasoning(self):
+        # Orphans benefit is permitted, so WFF PTC is not
+        self.assertTrue(self.isPermitted('isStudentAllowance'))
+        self.assertTrue(self.is_forbidden)
