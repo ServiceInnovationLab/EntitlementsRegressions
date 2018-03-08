f2f290c9ee3616a070d57c2b9bdfda553a2fec48
Brenda Wallace <shiny@cpan.org>
2018-03-09 12:03:11 +1300

    Removed passing benefit status, and added more complicated test

--- pytests/test_young_parent_payment.py
+++ pytests/test_young_parent_payment.py
@@ -31,14 +31,69 @@ class TestYoungParentPayment(Reasoner):
         "child": {
             "isDependent": True
         },
-        "benefit": {
-            "isOrphansBenefit": False,
-            "isUnsupportedChildsBenefit": False
-        },
         "threshold": {
             "income": {
                 "YoungParentPayment": True
             }
+        }
+    }
+
+    def test_reasoning(self):
+        self.assertTrue(self.is_permitted)
+
+
+class TestYoungParentPaymentJenRetyped(Reasoner):
+    key = 'isYoungParentPayment'
+    body = {
+        "applicant": {
+            "Age": 17,
+            "gaveBirthToThisChild": True,
+            "hasAccommodationCosts": False,
+            "hasLivedInNZfor2Years": True,
+            "hasMedicalCertificate": True,
+            "hasReceivedPaidParentalLeavePayment": False,
+            "hasSeriousDisability": False,
+            "holdsCommunityServicesCard": False,
+            "isMaintainingChild": True,
+            "isNZResident": True,
+            "isParent": True,
+            "isPrincipalCarer": True,
+            "normallyLivesInNZ": True,
+            "numberOfChildren": 1,
+            "receivesIncomeTestedBenefit": False,
+            "relationshipStatus": "single",
+        },
+        "child": {
+            "hasSeriousDisability": False,
+            "isDependent": True,
+            "Age": 0,
+            "hasMedicalCertification": True,
+            "isDependent": True
+        },
+        "children": {
+            "dependentsUnder14": 1
+        },
+        "threshold": {
+            "income": {
+                "AccommodationSupplement": True,
+                "ChildCareSubsidy": True,
+                "JobSeekerSupport": True,
+                "SoleParentSupport": True,
+                "SupportedLivingPayment": True,
+                "WorkingForFamiliesFamilyTaxCredit": True,
+                "WorkingForFamiliesInWorkTaxCredit": True,
+                "workingForFamiliesMinTaxCredit": True,
+                "workingForFamiliesParentalTaxCredit": True,
+                "YoungParentPayment": True
+            },
+            "isCommunityServicesCard": True,
+            "cash": {
+                "AccommodationSupplement": True,
+                "HomeHelp": True
+            }
+        },
+        "recipient": {
+            "prepareForEmployment": True
         }
     }
 
