59a37f751ee0dd0718f32af3ceb9efdc17ff5e33
Brenda Wallace <shiny@cpan.org>
2018-03-12 16:14:14 +1300

    Added SoleParent for single people

--- pytests/test_sole_parent_support.py
+++ pytests/test_sole_parent_support.py
@@ -1,20 +1,20 @@
 from . import Reasoner
 from pprint import pprint
-"""
-Benefit: Part 1B Sole Parent Support (eligibility in legislation):
-    If applicant.isParent
-        and applicant.isInadequatelySupportedByPartner
-        and applicant.isMaintainingChild
-        and applicant.isNZResident
-        and 20 ≤ applicant.Age
-        and child.Age < 14
-        and child.isDependent
-        and threshold.income.SoleParentSupport
-    then benefit.isSoleParentSupport is PERMITTED
-"""
 
 
-class TestSoleParentSupport(Reasoner):
+class TestSoleParentSupportNotSupported(Reasoner):
+    """
+    Benefit: Part 1B Sole Parent Support (eligibility in legislation):
+        If applicant.isParent
+            and applicant.isInadequatelySupportedByPartner
+            and applicant.isMaintainingChild
+            and applicant.isNZResident
+            and 20 ≤ applicant.Age
+            and child.Age < 14
+            and child.isDependent
+            and threshold.income.SoleParentSupport
+        then benefit.isSoleParentSupport is PERMITTED
+    """
     key = 'isSoleParentSupport'
 
     body = {
@@ -23,7 +23,9 @@ class TestSoleParentSupport(Reasoner):
             "isInadequatelySupportedByPartner": True,
             "isMaintainingChild": True,
             "isNZResident": True,
-            "Age": 20
+            "Age": 20,
+            "normallyLivesInNZ": True,
+            "hasLivedInNZfor2Years": True
         },
         "child": {
             "isDependent": True,
@@ -38,7 +40,52 @@ class TestSoleParentSupport(Reasoner):
                 "SoleParentSupport": True
             }
         }
+    }
 
+    def test_reasoning(self):
+        self.assertTrue(self.is_permitted)
+
+
+class TestSoleParentSupportSingle(Reasoner):
+    """
+    If applicant.relationshipStatus = "single"
+        and applicant.isParent
+        and applicant.isMaintainingChild
+        and applicant.isNZResident
+        and applicant.normallyLivesInNZ
+        and 20 ≤ applicant.Age
+        and child.Age < 14
+        and child.isDependent
+        and threshold.income.SoleParentSupport
+        and applicant.normallyLivesInNZ
+        and applicant.hasLivedInNZfor2Years
+    then benefit.isSoleParentSupport is PERMITTED
+    """
+    key = 'isSoleParentSupport'
+
+    body = {
+        "applicant": {
+            "isParent": True,
+            "relationshipStatus": "single",
+            "isMaintainingChild": True,
+            "isNZResident": True,
+            "Age": 20,
+            "normallyLivesInNZ": True,
+            "hasLivedInNZfor2Years": True
+        },
+        "child": {
+            "isDependent": True,
+            "Age": 5
+        },
+        "income": {
+            "ofApplicantAndSpouse": 200,
+            "fromMaintenancePayments": 200
+        },
+        "threshold": {
+            "income": {
+                "SoleParentSupport": True
+            }
+        }
     }
 
     def test_reasoning(self):
